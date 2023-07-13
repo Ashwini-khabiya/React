7f087583390a9a5cd514714232af469aad512e70
Ashwini Khabiya <ashwini-khabiya>
2023-07-14 00:10:51 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,29 +1,54 @@
-{/* <div id="root">
-    <div id="parent">
-        <div id="child1"> 
-            <h1>I am H1 tag!</h1>
-            <h1>I am H2 tag!</h1>
-        </div>
-        <div id="child2"> 
-            <h1>I am H1 tag!</h1>
-            <h1>I am H2 tag!</h1>
-        </div>
-    </div>
-</div> */}
-
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"},
-[React.createElement("div", {id:"child1"}, 
-[React.createElement("h1", {},"I am H1 tag!"),React.createElement("h2", {},"I am H2 tag!")]),
-React.createElement("div", {id:"child2"}, 
-[React.createElement("h1", {},"I am H1 tag!"),React.createElement("h2", {},"I am H2 tag!")])]);
+const JSXHeading = (
+  <React.Fragment>
+    <h1 className="heading" tabIndex="1">
+      This is my JSX heading 🛩️
+    </h1>
+    <h1>This is React Fragment!</h1>
+  </React.Fragment>
+);
 
+const JSXHeading2 = (
+    <>
+      <h1 className="heading" tabIndex="1">
+        This is my JSX heading 2 🛩️
+      </h1>
+      <h1>This is Empty tag!</h1>
+    </>
+  );
 
-// const heading = React.createElement("h1",{
-//     id:"heading"
-// }, "Hello from the React World!");
+const HeadingComponent1 = () => (
+  <h1>This is my Heading functional Component 1</h1>
+);
+
+const HeadingComponent1_1 = function () {
+  return <h1>This is my Heading functional Component 1.1</h1>;
+};
+
+const HeadingComponent2 = () => {
+  return <h1>This is my Heading functional Component 2</h1>;
+};
+
+//Component Composition: calling one component from another
+// Inject Javascript code inside JSX using {}
+
+const cnt = 10;
+
+const HeadingComponent3 = () => (
+  <div id="container">
+    {cnt + 100}
+
+    <HeadingComponent1 />
+    <HeadingComponent1_1></HeadingComponent1_1>
+    {HeadingComponent2()}
+
+    {JSXHeading}
+    <h1>This is my Heading functional Component 3</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent3 />);
