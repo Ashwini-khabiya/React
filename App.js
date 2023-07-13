bf804d5d562f128075ecf750c336a2213f423a44
Ashwini Khabiya <ashwini-khabiya>
2023-07-13 17:57:52 +0530

    episode 2

--- App.js
+++ App.js
@@ -11,6 +11,9 @@
     </div>
 </div> */}
 
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", {id:"parent"},
 [React.createElement("div", {id:"child1"}, 
 [React.createElement("h1", {},"I am H1 tag!"),React.createElement("h2", {},"I am H2 tag!")]),
