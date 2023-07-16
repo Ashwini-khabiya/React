cf3e97f6c8915e729c015e230b28a8ef4f90ba73
Ashwini Khabiya <ashwini-khabiya>
2023-07-16 23:40:29 +0530

    folder name corrected

--- src/componrnt/About.js
+++
@@ -1,9 +0,0 @@
-const About = () => {
-    return (
-        <div>
-            This is my about page!
-        </div>
-    );
-}
-
-export default About;
--- src/componrnt/Contact.js
+++
@@ -1,5 +0,0 @@
-const Contact = () => {
-  return <div className="contact-container">This is my contact page!</div>;
-};
-
-export default Contact;
