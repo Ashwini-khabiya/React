0667f7c13979ecbce1ae23d669cbbafcae7d4cbb
Ashwini Khabiya <ashwini-khabiya>
2023-07-16 23:22:01 +0530

    React-Router-Dom library

--- App.js
+++ App.js
@@ -1,54 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import Home from "./src/componrnt/Home";
+import About from "./src/componrnt/About";
+import Contact from "./src/componrnt/Contact";
+import Error from "./src/componrnt/Error";
 
-const JSXHeading = (
-  <React.Fragment>
-    <h1 className="heading" tabIndex="1">
-      This is my JSX heading 🛩️
-    </h1>
-    <h1>This is React Fragment!</h1>
-  </React.Fragment>
-);
-
-const JSXHeading2 = (
-    <>
-      <h1 className="heading" tabIndex="1">
-        This is my JSX heading 2 🛩️
-      </h1>
-      <h1>This is Empty tag!</h1>
-    </>
-  );
-
-const HeadingComponent1 = () => (
-  <h1>This is my Heading functional Component 1</h1>
-);
-
-const HeadingComponent1_1 = function () {
-  return <h1>This is my Heading functional Component 1.1</h1>;
-};
-
-const HeadingComponent2 = () => {
-  return <h1>This is my Heading functional Component 2</h1>;
-};
-
-//Component Composition: calling one component from another
-// Inject Javascript code inside JSX using {}
-
-const cnt = 10;
-
-const HeadingComponent3 = () => (
-  <div id="container">
-    {cnt + 100}
-
-    <HeadingComponent1 />
-    <HeadingComponent1_1></HeadingComponent1_1>
-    {HeadingComponent2()}
-
-    {JSXHeading}
-    <h1>This is my Heading functional Component 3</h1>
-  </div>
-);
+const AppConfig = createBrowserRouter([
+  {
+    path: '/',
+    element : <Home/>,
+    errorElement : <Error/>
+  },{
+    path: '/About',
+    element:<About/>
+  },{
+    path: '/Contact',
+    element: <Contact/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent3 />);
+root.render(<RouterProvider router={AppConfig}/>);
